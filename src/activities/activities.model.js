af80c944f940f5a2f47669059dcf10df13941a19
PierreCrepinDPS <pcrepin@agencedps.com>
2023-06-15 17:27:08 +0200

    modif description

--- src/activities/activities.model.js
+++ src/activities/activities.model.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
     "name": {type:String, unique:true, required:true},
     "types": {type:[String], enum:['Aquatique', 'Course', 'Jeux pour enfants', 'Vélo', 'BMX/Skate/Trotinette/Rollers', 'Football', 'Basketball', 'Pêche', 'Bowling', 'Lasergame', 'Paintball', 'Karting', 'Escalade', 'Piscine', 'Patinoire', 'Escape game', 'Plage', 'Musée','Monument','Jardins','Parc forestier','Parc aquatique',"Parc d'attractions",'Zoo','Boutique','Cinéma','Théâtre','Bar','Club','Bien-être','Médecine','Soins','Santé','Salles de jeux'], required:true},
     "likes": { type: [String], default: [] },
-    "description": { type: String, required: true },
+    "description": { type: Array, required: true },
     "image": {type: String},
     "position": {
         "longitude": {type: Number},
